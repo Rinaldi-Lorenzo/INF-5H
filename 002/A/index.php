fab69aa625bcea7a7b33e705e4ed67356e167aee
Rinaldi-Lorenzo <113595404+Rinaldi-Lorenzo@users.noreply.github.com>
2022-11-15 12:45:17 +0100

    Create index.php

---
+++ 002/A/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+
+	<title> LOGIN </title>
+
+</head>
+<body>
+	<form method="POST" action="login.php">
+		<label for="username" type="text">Username</label>
+		<input type="text" id="username" name="username" placeholder="Inserisci username">
+		<label for="password" type="text">Password</label>
+		<input type="password" id="password" name="password" placeholder="Inserisci password">
+		<input type="submit" value="Invia dati">
+	</form>
+</body>
+</html>
