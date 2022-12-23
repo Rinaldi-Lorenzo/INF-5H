4a3d4a9be14d6870fff94cde5441fa2a86b4f737
Rinaldi-Lorenzo <113595404+Rinaldi-Lorenzo@users.noreply.github.com>
2022-12-23 12:31:40 +0100

    Create index.php

---
+++ sessione/index.php
@@ -0,0 +1,23 @@
+<?php
+	/*
+	* mainpage <Gestione delle sessioni nelle applicazioni web>
+	*
+	* 
+	* @author <Rinaldi Lorenzo>
+	* @date <19/12/2022> 
+	* @version 1.0 <19/12/2022> Versione iniziale
+	* @version 1.1 <23/23/2022>
+	*/
+?>
+
+<html>
+	<head>
+		<title> Homepage </title>
+	</head>
+		<body>
+			<h1>WEBAPP1</h1>
+			<h2>Gestione delle sessioni nelle applicazioni web</h2>
+			<a href="riservata.php">Vai all'area riservata del sito</a><br>
+			<a href="login.php">Login</a><br>
+		</body>
+	</html>
